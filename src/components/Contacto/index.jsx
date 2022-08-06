0f6a93425451b5f4bc779f2b1bba03c809f4011f
stevenLucano <st.lucano1205@gmail.com>
2022-08-06 17:28:11 -0500

    Formulario estilizado

--- src/components/Contacto/index.jsx
+++ src/components/Contacto/index.jsx
@@ -10,6 +10,7 @@ const Contacto = () => {
     e.preventDefault();
 
     /* Tener en cuenta que vite maneja import.meta.env y las variables deben iniciar con VITE_KEY */
+    /* Se utilizo vite-plugin-environment para poder usar variables env en VERCEL*/
     emailjs
       .sendForm(
         process.env.SERVICE_ID,
@@ -25,37 +26,48 @@ const Contacto = () => {
           console.log(error.text);
         }
       );
+    // console.log(form.current);
   };
-
-  // useEffect(() => {
-  //   console.log("que onda");
-  //   console.log(process.env.VITE_PUBLIC_KEY);
-  // }, []);
 
   return (
     <section id="contacto">
-      <h2>Contactame!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, placeat?
-      </p>
-      <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name="nombre" id="nombre" placeholder="Nombre" />
-        <input
-          type="text"
-          name="email"
-          id="email"
-          placeholder="Correo electrónico"
-        />
-        <input type="text" name="asunto" id="asunto" placeholder="Asunto" />
-        <textarea
-          name="mensaje"
-          id=""
-          cols="30"
-          rows="10"
-          placeholder="Mensaje..."
-        ></textarea>
-        <input type="submit" value="Enviar" />
-      </form>
+      <div className="contact-container">
+        <h2 className="contact-title">Contactame</h2>
+        <p className="contact-content">
+          Me gusta afrontar cualquier tipo de desafío. Así que si deseas ponerme
+          a prueba o tienes alguna duda, no dudes en contactarme.
+        </p>
+        <form ref={form} onSubmit={sendEmail} autoComplete="off">
+          <div className="inputBox">
+            <input type="text" name="nombre" required />
+            <span className="text">Nombre</span>
+            <span className="shadow"></span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="email" required />
+            <span className="text">Correo electrónico</span>
+            <span className="shadow"></span>
+          </div>
+          <div className="inputBox">
+            <input type="text" name="asunto" id="nombre" required />
+            <span className="text">Asunto</span>
+            <span className="shadow"></span>
+          </div>
+          <div className="inputBox textarea">
+            <textarea name="mensaje" required></textarea>
+            <span className="text">Escribe tu mensaje aquí...</span>
+            <span className="shadow"></span>
+          </div>
+          <div className="button-submit">
+            <button type="submit">
+              <div>
+                <span className="bg"></span>
+                <span className="text">Enviar</span>
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
